57ba7e821eab253faa223c4e2a2330eae0d487ea
Jeremy Collins <jeremy@jeremycollins.me>
2017-08-21 23:20:48 -0500

    Added logic to suggest a similar word if input is not found

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
+    word = word.lower()
+
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean \"%s\" instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "\"" + word + "\"" + " does not exist in the dictionary."
 
