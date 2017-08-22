3f6f31f56c4d38daef472f264efa940a64dc61ed
Jeremy Collins <jeremy@jeremycollins.me>
2017-08-21 23:31:07 -0500

    Created logic to handle suggested word scenarios.

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ def translate(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean \"%s\" instead?" % get_close_matches(word, data.keys())[0]
+        yn = input("Did you mean \"%s\" instead? Enter 'Y' if yes, or 'N' if no: " % get_close_matches(word, data.keys())[0])
+        if yn.lower() == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn.lower() == "n":
+            return "Sorry, \"" + word + "\"" + " does not exist in the dictionary."
+        else:
+            return "We didn't understand your entry."
     else:
-        return "\"" + word + "\"" + " does not exist in the dictionary."
+        return "Sorry, \"" + word + "\"" + " does not exist in the dictionary."
 
 word = input("Enter word: ")
 
